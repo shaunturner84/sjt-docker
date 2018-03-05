556f93d9df307bdeca4ee9d26475032831c22b32
shaunturner84 <shaunturner84@gmail.com>
2018-03-05 16:37:57 +0000

    Added SSL by default to the sjt-pgsql image

--- pgsql/README.md
+++ pgsql/README.md
@@ -1,7 +1,7 @@
-= INTRODUCTION =
+INTRODUCTION:
 This image sjt-pgsql implements a basic container that runs an instance of postgresql server. 
 
-= INSTRUCTIONS =
+INSTRUCTIONS:
 This inherits from image sjt-pgsql:latest so first you'll need to build that (if you haven't already):
 
    docker build . -t sjt-base
@@ -10,4 +10,27 @@ Once completed you can build sjt-pgsql
 
    docker build . -t sjt-pgsql
 
+once the container is built you can run a copy of it using the following command:
 
+   docker run -it sjt-pgsql:latest
+
+In the logs you will be able to see the initial password for the sjtadmin account:
+
+   docker logs <container name>
+   
+example: initial sjtadmin password: SQQvNSSYe9  - please change ASAP
+
+You will also need to download the client certificate and key before you can connect to it using psql (or anything else)
+
+   docker cp <container name>:/tmp/client.crt /tmp/
+   docker cp <container name>:/tmp/client.key /tmp/
+
+you can get the ip address of the container from:
+
+   docker inspect -f "{{ .NetworkSettings.IPAddress }}" <container name>
+
+to connect using psql:
+
+   psql "sslmode=require host=172.17.0.2 user=sjtadmin dbname=postgres sslcert=/tmp/client.crt sslkey=/tmp/client.key"
+
+Once logged in don't forget to change the sjtadmin password.
--- pgsql/bin/start-pgsql.sh
+++ pgsql/bin/start-pgsql.sh
@@ -14,8 +14,83 @@ then
     psql -c "CREATE ROLE sjtadmin WITH LOGIN PASSWORD '$newpasswd' SUPERUSER;"
     echo "initial sjtadmin password: $newpasswd  - please change ASAP";   
     
-    echo "Stopping postgresql" >l> $logfile;
+    
+    echo "Stopping postgresql" >> $logfile;
     pg_ctl stop -D /var/lib/pgsql/data -w;
+    
+    
+    cd /var/lib/pgsql/data;
+    
+    echo "Generating Root CA Certificate" >> $logfile
+    openssl req -new \
+                -nodes \
+                -text \
+                -out root.csr \
+                -keyout root.key \
+                -subj "/CN=$HOSTNAME-Root";
+    chmod og-rwx root.key;
+                
+    echo "Signing the request" >> $logfile
+    openssl x509 -req \
+                 -in root.csr \
+                 -text \
+                 -days 365 \
+                 -extfile /etc/pki/tls/openssl.cnf \
+                 -extensions v3_ca \
+                 -signkey root.key \
+                 -out root.crt;
+                 
+    echo "Generating Server SSL Certificate" >> $logfile;
+    openssl req -new \
+                -nodes \
+                -text \
+                -out server.csr \
+                -keyout server.key \
+                -subj "/CN=$HOSTNAME";
+    chmod og-rwx server.key;
+    openssl x509 -req \
+                 -in server.csr \
+                 -text \
+                 -days 365 \
+                 -CA root.crt \
+                 -CAkey root.key \
+                 -CAcreateserial \
+                 -out server.crt;
+                 
+    echo "Generating Client SSL Certificate" >> $logfile;
+    openssl req -new \
+                -nodes \
+                -text \
+                -out client.csr \
+                -keyout client.key \
+                -subj "/CN=$HOSTNAME-client";
+    chmod og-rwx client.key;
+    openssl x509 -req \
+                 -in client.csr \
+                 -text \
+                 -days 365 \
+                 -CA root.crt \
+                 -CAkey root.key \
+                 -CAcreateserial \
+                 -out client.crt;
+    
+    mv client.key /tmp
+    mv client.crt /tmp
+                 
+    touch /var/lib/pgsql/data/root.crl;
+    chown postgres:postgres /var/lib/pgsql/data/root.crl;
+    
+    echo "Modifying postgresql.conf" >> $logfile;
+    echo "ssl = on" >> postgresql.conf;
+    #echo "ssl_cert_file = '$PGDATA/server.crt'" >> postgresql.conf;
+    #echo "ssl_key_file = '$PGDATA/server.key'" >> postgresql.conf;
+    #echo "ssl_ca_file = '$PGDATA/root.crt'" >> postgresql.conf;
+    #echo "ssl_crl_file = '$PGDATA/root.crl'" >> postgresql.conf;
+    
+    echo "Modifying pg_hba.conf" >> $logfile;
+    echo "local   all         postgres                           trust" > pg_hba.conf
+    echo "hostssl    all         all         0.0.0.0/0              md5 clientcert=1" >> pg_hba.conf
+    chown postgres:postgres pg_hba.conf;
     
     echo "first time setup completed";
 fi
